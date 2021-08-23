3dfabcfa0cd06ef4cae029da5351fa3a2441d327
dedi <dediirwansyah1925@gmail.com>
2021-08-23 10:59:23 +0700

    membuat halaman di router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Product from "../views/product/index.vue";
+import ProductDetail from "../views/product/Detail.vue";
+import Cart from "../views/product/index.vue";
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Product",
+    component: Product,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/product/:id",
+    name: "ProductDetail",
+    component: ProductDetail,
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: Cart,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/product/Detail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><h1>detail product</h1></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
